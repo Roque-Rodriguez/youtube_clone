b494894f58833303df9581968a325936012e9f49
unknown <Roque.Rodriguez02@gmail.com>
2023-07-31 14:12:56 -0400

    Added Comment and Replies Models + Installed MysqlClient

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
-    'cars.apps.CarsConfig'  # This app is for example use only
-]
+    'cars.apps.CarsConfig',  # This app is for example use only
+    'Comments',
+    'replies',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
