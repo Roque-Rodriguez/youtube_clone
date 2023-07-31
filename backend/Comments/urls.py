206edf607a52296e8cfa6cef0d017bfd97a8914b
unknown <Roque.Rodriguez02@gmail.com>
2023-07-31 14:57:51 -0400

    Added code to Models, Serializers, url, and Views

---
+++ backend/Comments/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import get_comments_by_video_id, create_comment
+
+urlpatterns = [
+    path('comments/<str:video_id>/', get_comments_by_video_id),
+    path('comments/', create_comment),
+]
--- backend/Comments/views.py
+++ backend/Comments/views.py
@@ -1,1 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Comment
+from .serializers import CommentSerializer
+
+@api_view(['GET'])
+@permission_classes([AllowAny])  # Allow any user to access this endpoint
+def get_comments_by_video_id(request, video_id):
+    comments = Comment.objects.filter(video_id=video_id)
+    serializer = CommentSerializer(comments, many=True)  # Serialize multiple objects
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])  # Require JWT authorization for this endpoint
+def create_comment(request):
+    # Access the user using `request.user`
+    user = request.user
+
+    # Deserialize the request data and create a new comment
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=user)  # Save the user along with the comment
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
